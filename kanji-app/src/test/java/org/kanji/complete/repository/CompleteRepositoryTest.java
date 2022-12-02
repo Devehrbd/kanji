0092689c75a0fa4ada16d4bb2643ea2419738adf
J-main <J-main@J-018.mshome.net>
2022-12-02 17:40:34 +0900

    221202

---
+++ kanji-app/src/test/java/org/kanji/complete/repository/CompleteRepositoryTest.java
@@ -0,0 +1,14 @@
+package org.kanji.complete.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CompleteRepositoryTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
